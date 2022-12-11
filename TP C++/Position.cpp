af70dee6b36610bf679066b430bb0c3fb18d5337
SpacioRift <107812609+SpacioRift@users.noreply.github.com>
2022-12-11 11:39:32 +0100

    Update Position.cpp

--- TP C++/Position.cpp
+++ TP C++/Position.cpp
@@ -1,52 +1,3 @@
-////
-////  Position.cpp
-////  TP-robots
-////
-////  Created by egrislin on 13/10/2022.
-////
-//
-//#include "Position.hpp"
-//#include <iostream>
-//
-///*
-// constructeur
-// */
-//Position::Position() {
-//    this->estLibre = true;
-//}
-//
-///*
-// renvoie true si la position etait libre et l'occupation a ete realisee
-// false sinon
-// */
-//bool Position::occuper(char _occupant){
-//    if (this->estLibre){
-//        this->estLibre = false;
-//        this->occupant = _occupant;
-//        return true;
-//    } else {
-//        std::cout<<" La position " << this->x<< ","
-//        << this->y <<" est d�j� occup�e\n";
-//        return false;
-//    }
-//}
-//
-///* renvoie true si la position etait occupee et a ete liberee
-// */
-//bool Position::liberer(){
-//    // A FAIRE
-//}
-//
-//int Position::getX(){ return this->x;}
-//int Position::getY(){ return this->y;}
-//bool Position::getEstLibre(){
-//    // A FAIRE
-//}
-//char Position::getOccupant(){
-//    // A FAIRE
-//}
-//
-
 #include "Position.hpp"
 #include <iostream>
 
@@ -68,7 +19,7 @@ bool Position::occuper(char _occupant){
         return true;
     } else {
         std::cout<<" La position " << this->x<< ","
-        << this->y <<" est d�j� occup�e\n";
+        << this->y <<" est déjà occupée\n";
         return false;
     }
 }
@@ -82,7 +33,7 @@ bool Position::liberer(){
         return true;
     }else {
         std::cout<<"La position " << this->x << ","
-        << this->y << " est d�j� occup�e \n";
+        << this->y << " est déjà occupée \n";
         return false;
     }
 }
@@ -101,4 +52,3 @@ bool Position::getEstLibre(){
 char Position::getOccupant(){
     return this->occupant;
 }
-
