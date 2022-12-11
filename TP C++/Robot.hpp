83ce4c3c44d14a7b215ce706af131d7318548189
SpacioRift <107812609+SpacioRift@users.noreply.github.com>
2022-12-11 11:36:00 +0100

    Update Robot.hpp

--- TP C++/Robot.hpp
+++ TP C++/Robot.hpp
@@ -1,16 +1,10 @@
-//
-//  Robot.hpp
-//  TP-robots
-//
-//  Created by egrislin on 13/10/2022.
-//
-
 #ifndef Robot_hpp
 #define Robot_hpp
 
 #include "Position.hpp"
 #include "Environnement.hpp"
 #include "Occupant.hpp"
+#include "Box.hpp"
 
 class Robot : public Occupant{
     int vitesse;
@@ -22,8 +16,8 @@ public:
     bool avancer(Environnement &); // avancer d'un nombre de cases = la vitesse dans la direction courante
     void operator+(int);//augmenter la vitesse
     void operator-(int);//diminuer la vitesse
+    bool tenir(Robot &, Environnement &, Box &);
+    bool lacher(Robot &, Environnement &, Box &);
 };
 
 #endif /* Robot_hpp */
-
-
